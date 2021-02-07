07eb45ec08e6ba8c475a8d050211a5deca721eb4
Won Cho <won.cho.ai@gamil.com>
2021-02-07 17:33:39 -0500

    Create DecoratorDemo.java

---
+++ src/designpattern/decorator/DecoratorDemo.java
@@ -0,0 +1,18 @@
+package designpattern.decorator;
+
+public class DecoratorDemo {
+	public static void main(String[] args) {
+		Beverage beverage = new Espresso();
+		System.out.println(beverage.getDescription() + " $" + beverage.cost());
+		
+		Beverage beverage2 = new Espresso();
+		beverage2 = new Mocha(beverage2);
+		beverage2 = new Soy(beverage2);
+		System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+		
+		Beverage beverage3 = new HouseBlend();
+		beverage3 = new Mocha(beverage3);
+		beverage3 = new Soy(beverage3);
+		System.out.println(beverage3.getDescription() + " $" + beverage3.cost());
+	}
+}
