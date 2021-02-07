4cc590b49c7b0ef7f02960570bdc544a040b9367
Won Cho <won.cho.ai@gamil.com>
2021-02-07 17:33:57 -0500

    Create Mocha.java

---
+++ src/designpattern/decorator/Mocha.java
@@ -0,0 +1,21 @@
+package designpattern.decorator;
+
+public class Mocha extends CondimentDecorator {
+	Beverage beverage;
+	
+	public Mocha(Beverage beverage) {
+		this.beverage = beverage;
+	}
+	
+	@Override
+	public String getDescription() {
+		return beverage.getDescription() + ", Mocha";
+	}
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return beverage.cost() + .50;
+	}
+
+}
